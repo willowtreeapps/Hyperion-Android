21f900dc666efea1c2950c9646a1ff6f9d448a9b
Ian Thomas <toxicbakery@gmail.com>
2018-04-03 16:33:18 -0400

    Update to resolve PR issues - Preference view activity sets a specific theme - Added toolbar and home back arrow - Created a theme and colors for the plugin

--- hyperion-shared-preferences/src/main/java/com/willowtreeapps/hyperion/sharedpreferences/SharedPreferencesViewerActivity.java
+++ hyperion-shared-preferences/src/main/java/com/willowtreeapps/hyperion/sharedpreferences/SharedPreferencesViewerActivity.java
@@ -2,7 +2,9 @@ package com.willowtreeapps.hyperion.sharedpreferences;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.willowtreeapps.hyperion.plugin.v1.HyperionIgnore;
 
@@ -13,6 +15,16 @@ public class SharedPreferencesViewerActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hsp_activity_shared_preferences_viewer);
+        setSupportActionBar((Toolbar) findViewById(R.id.hsp_toolbar));
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
