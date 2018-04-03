7c62457dd949d6be054430ade86d6d550bcb0600
Ian Thomas <toxicbakery@gmail.com>
2018-04-03 15:18:59 -0400

    Added missing ignore to preference viewer activity

--- hyperion-shared-preferences/src/main/java/com/willowtreeapps/hyperion/sharedpreferences/SharedPreferencesViewerActivity.java
+++ hyperion-shared-preferences/src/main/java/com/willowtreeapps/hyperion/sharedpreferences/SharedPreferencesViewerActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.willowtreeapps.hyperion.plugin.v1.HyperionIgnore;
+
+@HyperionIgnore
 public class SharedPreferencesViewerActivity extends AppCompatActivity {
 
     @Override
