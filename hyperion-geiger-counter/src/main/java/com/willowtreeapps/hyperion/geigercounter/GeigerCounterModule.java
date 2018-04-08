e7e17fe472dd5bc36718c4f681e87e5711ce4a6d
Kevin Conner <connerk@gmail.com>
2018-04-08 00:21:00 -0400

    Include code and sound effect from Geiger counter prototype

--- hyperion-geiger-counter/src/main/java/com/willowtreeapps/hyperion/geigercounter/GeigerCounterModule.java
+++ hyperion-geiger-counter/src/main/java/com/willowtreeapps/hyperion/geigercounter/GeigerCounterModule.java
@@ -1,7 +1,13 @@
 package com.willowtreeapps.hyperion.geigercounter;
 
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Choreographer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,16 +17,35 @@ import com.willowtreeapps.hyperion.plugin.v1.OverlayContainer;
 import com.willowtreeapps.hyperion.plugin.v1.PluginModule;
 
 class GeigerCounterModule extends PluginModule
-        implements View.OnClickListener, OnOverlayViewChangedListener {
+        implements View.OnClickListener, OnOverlayViewChangedListener, Choreographer.FrameCallback {
 
     private static final String OVERLAY_TAG = "geiger_counter_overlay";
     private OverlayContainer overlay;
+
+    // Menu item in the Hyperion plugin list
     private View view;
+
+    private SoundPool soundPool;
+    private int tickSoundID;
+    private double hardwareFrameIntervalSeconds;
+    private long lastTimestampNanoseconds = -1;
 
     @Override
     protected void onCreate() {
         overlay = getExtension().getOverlayContainer();
         overlay.addOnOverlayViewChangedListener(this);
+
+        Activity activity = getExtension().getActivity();
+
+        soundPool = new SoundPool(1, AudioManager.STREAM_SYSTEM, 0);
+        try {
+            AssetFileDescriptor afd = activity.getAssets().openFd("sounds/GeigerCounterTick.wav");
+            tickSoundID = soundPool.load(afd, 1);
+        } catch (Exception exception) {
+            Log.e("Geiger Counter", exception.toString());
+        }
+        hardwareFrameIntervalSeconds = 1.0 / activity.getWindowManager().getDefaultDisplay().getRefreshRate();
+        Choreographer.getInstance().postFrameCallback(this);
     }
 
     @Nullable
@@ -38,6 +63,8 @@ class GeigerCounterModule extends PluginModule
     @Override
     protected void onDestroy() {
         overlay.removeOnOverlayViewChangedListener(this);
+
+        Choreographer.getInstance().removeFrameCallback(this);
     }
 
     @Override
@@ -55,5 +82,38 @@ class GeigerCounterModule extends PluginModule
     @Override
     public void onOverlayViewChanged(@Nullable View view) {
         this.view.setSelected(view != null && OVERLAY_TAG.equals(view.getTag()));
+    }
+
+    // Choreographer.FrameCallback
+
+    @Override
+    public void doFrame(long timestampNanoseconds) {
+        // Ideally, frame intervals will be exactly 1x the hardware interval,
+        // and 2x means you definitely dropped one frame. So 1.5x is our point of comparison.
+        double droppedFrameIntervalSeconds = hardwareFrameIntervalSeconds * 1.5;
+
+        long frameIntervalNanoseconds = timestampNanoseconds - lastTimestampNanoseconds;
+        if (0 < lastTimestampNanoseconds) {
+            // Compare if we have received at least two frame callbacks
+            double frameIntervalSeconds = frameIntervalNanoseconds / 1_000_000_000.0;
+            if (droppedFrameIntervalSeconds < frameIntervalSeconds) {
+                soundPool.play(tickSoundID, 1, 1, 1, 0, 1);
+
+                int frameIntervalMilliseconds = (int) (frameIntervalSeconds * 1000);
+                int hardwareFrameIntervalMilliseconds = (int) (hardwareFrameIntervalSeconds * 1000);
+
+                StringBuilder message = new StringBuilder();
+                message.append("Dropped frame: ");
+                message.append(frameIntervalMilliseconds);
+                message.append("ms, out of ");
+                message.append(hardwareFrameIntervalMilliseconds);
+                message.append("ms");
+
+                Log.d("Geiger Counter", message.toString());
+            }
+        }
+
+        lastTimestampNanoseconds = timestampNanoseconds;
+        Choreographer.getInstance().postFrameCallback(this);
     }
 }
