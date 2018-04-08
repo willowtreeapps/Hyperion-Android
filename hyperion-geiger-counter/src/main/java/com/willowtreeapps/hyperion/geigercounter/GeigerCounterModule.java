131f4dbba98d65e31d6730bbf696fd1c0175f0e2
Kevin Conner <connerk@gmail.com>
2018-04-07 23:18:09 -0400

    Duplicate the measurement plugin as a Geiger counter plugin

---
+++ hyperion-geiger-counter/src/main/java/com/willowtreeapps/hyperion/geigercounter/GeigerCounterModule.java
@@ -0,0 +1,59 @@
+package com.willowtreeapps.hyperion.geigercounter;
+
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.willowtreeapps.hyperion.plugin.v1.OnOverlayViewChangedListener;
+import com.willowtreeapps.hyperion.plugin.v1.OverlayContainer;
+import com.willowtreeapps.hyperion.plugin.v1.PluginModule;
+
+class GeigerCounterModule extends PluginModule
+        implements View.OnClickListener, OnOverlayViewChangedListener {
+
+    private static final String OVERLAY_TAG = "geiger_counter_overlay";
+    private OverlayContainer overlay;
+    private View view;
+
+    @Override
+    protected void onCreate() {
+        overlay = getExtension().getOverlayContainer();
+        overlay.addOnOverlayViewChangedListener(this);
+    }
+
+    @Nullable
+    @Override
+    public View createPluginView(@NonNull LayoutInflater layoutInflater, @NonNull ViewGroup parent) {
+        View view = layoutInflater.inflate(R.layout.hgc_item_plugin, parent, false);
+        view.setOnClickListener(this);
+
+        View overlayView = overlay.getOverlayView();
+        view.setSelected(overlayView != null && OVERLAY_TAG.equals(overlayView.getTag()));
+        this.view = view;
+        return view;
+    }
+
+    @Override
+    protected void onDestroy() {
+        overlay.removeOnOverlayViewChangedListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        View currentOverlay = overlay.getOverlayView();
+        if (currentOverlay == null || !OVERLAY_TAG.equals(currentOverlay.getTag())) {
+            View newOverlay = new MeasurementOverlayView(getContext());
+            newOverlay.setTag(OVERLAY_TAG);
+            overlay.setOverlayView(newOverlay);
+        } else {
+            overlay.removeOverlayView();
+        }
+    }
+
+    @Override
+    public void onOverlayViewChanged(@Nullable View view) {
+        this.view.setSelected(view != null && OVERLAY_TAG.equals(view.getTag()));
+    }
+}
