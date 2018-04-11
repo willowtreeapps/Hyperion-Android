5a0ba3780c76e93fff4295c408f3e4c7ec6885f5
Kurian Vithayathil <k.vithayathil@gmail.com>
2018-04-11 19:34:27 -0400

    - Show confirmation dialog when phoenix is selected - Options toggle in dialog

--- hyperion-phoenix/src/main/java/com/willowtreeapps/hyperion/phoenix/PhoenixModule.java
+++ hyperion-phoenix/src/main/java/com/willowtreeapps/hyperion/phoenix/PhoenixModule.java
@@ -2,7 +2,10 @@ package com.willowtreeapps.hyperion.phoenix;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -43,10 +46,71 @@ class PhoenixModule extends PluginModule implements OnMenuStateChangedListener {
             @Override
             public void onClick(View v) {
                 final Activity activity = getExtension().getActivity();
-                ProcessPhoenix.triggerRebirth(activity);
+                confirmRebirth(activity);
             }
         });
         return view;
+    }
+
+
+    private void confirmRebirth(final Activity activity) {
+        new AlertDialog.Builder(activity)
+                .setTitle(R.string.hp_pheonix_options_title)
+                .setMultiChoiceItems(R.array.hp_phoenix_options,
+                        loadPhoenixOptions(activity.getResources()),
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                setOptionSelection(which, isChecked);
+                            }
+                        })
+                .setPositiveButton(R.string.hp_dialog_confirmation, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ProcessPhoenix.triggerRebirth(activity);
+                    }
+                })
+                .setNegativeButton(android.R.string.cancel, null)
+                .create()
+                .show();
+    }
+
+    private boolean[] loadPhoenixOptions(@NonNull Resources resources) {
+        final String[] selection
+                = resources.getStringArray(R.array.hp_phoenix_options);
+        final boolean[] options = new boolean[selection.length];
+
+        for (int i = 0; i < selection.length; i++) {
+            switch (i) {
+                case 0:
+                    options[i] = ProcessPhoenix.getOptions().shouldClearCache();
+                    break;
+                case 1:
+                    options[i] = ProcessPhoenix.getOptions().shouldClearData();
+                    break;
+                case 2:
+                    options[i] = ProcessPhoenix.getOptions().shouldRestartSelf();
+                    break;
+            }
+        }
+        return options;
+    }
+
+    private void setOptionSelection(int selectionIndex, boolean selected) {
+        final RebirthOptions.Builder optionsBuilder
+                = ProcessPhoenix.getOptions().builder();
+        switch (selectionIndex) {
+            case 0:
+                optionsBuilder.clearCache(selected);
+                break;
+            case 1:
+                optionsBuilder.clearData(selected);
+                break;
+            case 2:
+                optionsBuilder.restartSelf(selected);
+                break;
+        }
+        ProcessPhoenix.setOptions(optionsBuilder.build());
     }
 
     @Override
