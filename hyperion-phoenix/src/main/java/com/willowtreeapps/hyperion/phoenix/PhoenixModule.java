aabaa73b9327b46fa67a122a17bbaa2f9eb09c83
Kurian Vithayathil <k.vithayathil@gmail.com>
2018-04-11 20:18:27 -0400

    - Removing existing popup menu functionality

--- hyperion-phoenix/src/main/java/com/willowtreeapps/hyperion/phoenix/PhoenixModule.java
+++ hyperion-phoenix/src/main/java/com/willowtreeapps/hyperion/phoenix/PhoenixModule.java
@@ -1,47 +1,21 @@
 package com.willowtreeapps.hyperion.phoenix;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.PopupMenu;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.willowtreeapps.hyperion.plugin.v1.HyperionMenu;
-import com.willowtreeapps.hyperion.plugin.v1.MenuState;
-import com.willowtreeapps.hyperion.plugin.v1.OnMenuStateChangedListener;
 import com.willowtreeapps.hyperion.plugin.v1.PluginModule;
 
-class PhoenixModule extends PluginModule implements OnMenuStateChangedListener {
-
-    private HyperionMenu hyperionMenu;
-    private PopupMenu popupMenu;
-
-    @Override
-    protected void onCreate() {
-        hyperionMenu = getExtension().getHyperionMenu();
-        if (hyperionMenu != null) {
-            hyperionMenu.addOnMenuStateChangedListener(this);
-        }
-    }
+class PhoenixModule extends PluginModule {
 
     @Override
     public View createPluginView(@NonNull LayoutInflater layoutInflater, @NonNull ViewGroup parent) {
         final View view = layoutInflater.inflate(R.layout.hp_item_plugin, parent, false);
-        final View optionsButton = view.findViewById(R.id.button_more);
-        optionsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showMenu(v);
-            }
-        });
-
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -111,74 +85,5 @@ class PhoenixModule extends PluginModule implements OnMenuStateChangedListener {
                 break;
         }
         ProcessPhoenix.setOptions(optionsBuilder.build());
-    }
-
-    @Override
-    protected void onDestroy() {
-        hyperionMenu.removeOnMenuStateChangedListener(this);
-    }
-
-    private void showMenu(View anchor) {
-        final Context context = anchor.getContext();
-        if (popupMenu != null) {
-            popupMenu.dismiss();
-        }
-        popupMenu = new PopupMenu(context, anchor);
-        popupMenu.inflate(R.menu.hp_menu_options);
-
-        final Menu menu = popupMenu.getMenu();
-        final MenuItem clearCacheItem = menu.findItem(R.id.menu_item_clear_cache);
-        final MenuItem clearDataItem = menu.findItem(R.id.menu_item_clear_data);
-        final MenuItem restartSelfItem = menu.findItem(R.id.menu_item_restart_self);
-
-        final RebirthOptions options = ProcessPhoenix.getOptions();
-        clearCacheItem.setChecked(options.shouldClearCache());
-        clearDataItem.setChecked(options.shouldClearData());
-        restartSelfItem.setChecked(options.shouldRestartSelf());
-
-        clearCacheItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                item.setChecked(!item.isChecked());
-                final RebirthOptions options = ProcessPhoenix.getOptions().builder()
-                        .clearCache(item.isChecked())
-                        .build();
-                ProcessPhoenix.setOptions(options);
-                return true;
-            }
-        });
-
-        clearDataItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                item.setChecked(!item.isChecked());
-                final RebirthOptions options = ProcessPhoenix.getOptions().builder()
-                        .clearData(item.isChecked())
-                        .build();
-                ProcessPhoenix.setOptions(options);
-                return true;
-            }
-        });
-
-        restartSelfItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                item.setChecked(!item.isChecked());
-                final RebirthOptions options = ProcessPhoenix.getOptions().builder()
-                        .restartSelf(item.isChecked())
-                        .build();
-                ProcessPhoenix.setOptions(options);
-                return true;
-            }
-        });
-
-        popupMenu.show();
-    }
-
-    @Override
-    public void onMenuStateChanged(@NonNull MenuState menuState) {
-        if (menuState == MenuState.CLOSING && popupMenu != null) {
-            popupMenu.dismiss();
-        }
     }
 }
