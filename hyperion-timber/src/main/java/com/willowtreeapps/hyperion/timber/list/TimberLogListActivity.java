f4863a5b07ff8ecf7f7e12a8b5f5dd8c28b940ec
John Carlson <jawnnypoo@gmail.com>
2019-05-17 21:44:49 -0500

    Add filter UI

--- hyperion-timber/src/main/java/com/willowtreeapps/hyperion/timber/list/TimberLogListActivity.java
+++ hyperion-timber/src/main/java/com/willowtreeapps/hyperion/timber/list/TimberLogListActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.willowtreeapps.hyperion.plugin.v1.HyperionIgnore;
 import com.willowtreeapps.hyperion.timber.R;
@@ -23,25 +27,63 @@ import timber.log.Timber;
 
 @HyperionIgnore
 public class TimberLogListActivity extends AppCompatActivity implements View.OnClickListener {
+    Toolbar mToolbar;
+    Toolbar mFilterToolbar;
+    EditText mFilterEditText;
     Button mShareButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tmb_activity_timber_list);
-        setSupportActionBar((Toolbar) findViewById(R.id.tmb_toolbar));
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
+        mToolbar = findViewById(R.id.tmb_toolbar);
+        mToolbar.inflateMenu(R.menu.tmb_filter);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (item.getItemId() == R.id.menu_item_filter) {
+                    fadeIn(mFilterToolbar);
+                    return true;
+                }
+                return false;
+            }
+        });
+        mFilterToolbar = findViewById(R.id.tmb_filter_toolbar);
+        mFilterEditText = findViewById(R.id.tmb_filter);
+
+        CircularBuffer<LogItem> logItemQueue = getLogItemQueue();
+        final TimberLogListAdapter adapter = new TimberLogListAdapter(logItemQueue);
+        mFilterEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                adapter.applyFilter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+        mFilterToolbar.setNavigationIcon(R.drawable.tmb_ic_close);
+        mFilterToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fadeOut(mFilterToolbar);
+                mFilterEditText.getText().clear();
+                adapter.applyFilter(null);
+            }
+        });
 
         mShareButton = findViewById(R.id.button_share);
         mShareButton.setOnClickListener(this);
 
-        CircularBuffer<LogItem> logItemQueue = getLogItemQueue();
         RecyclerView recyclerView = findViewById(R.id.tmb_recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new TimberLogListAdapter(logItemQueue));
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
@@ -87,5 +129,25 @@ public class TimberLogListActivity extends AppCompatActivity implements View.OnC
                 // most general text sharing MIME type
                 .setType("text/plain")
                 .startChooser();
+    }
+
+    private void fadeIn(View view) {
+        view.setVisibility(View.VISIBLE);
+        view.setAlpha(0.0f);
+        view.animate()
+                .alpha(1.0f);
+    }
+
+    private void fadeOut(final View view) {
+        long duration = 250L;
+        view.animate()
+                .setDuration(duration)
+                .alpha(0.0f);
+        view.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                view.setVisibility(View.GONE);
+            }
+        }, duration);
     }
 }
--- hyperion-timber/src/main/java/com/willowtreeapps/hyperion/timber/list/TimberLogListAdapter.java
+++ hyperion-timber/src/main/java/com/willowtreeapps/hyperion/timber/list/TimberLogListAdapter.java
@@ -1,6 +1,7 @@
 package com.willowtreeapps.hyperion.timber.list;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,14 @@ import com.willowtreeapps.hyperion.timber.R;
 import com.willowtreeapps.hyperion.timber.model.CircularBuffer;
 import com.willowtreeapps.hyperion.timber.model.LogItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class TimberLogListAdapter extends RecyclerView.Adapter<TimberLogViewHolder> {
 
     private final CircularBuffer<LogItem> logItems;
+    private String filter;
+    private List<LogItem> filteredLogItems;
 
     TimberLogListAdapter(CircularBuffer<LogItem> logItems) {
         this.logItems = logItems;
@@ -28,12 +34,26 @@ class TimberLogListAdapter extends RecyclerView.Adapter<TimberLogViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull TimberLogViewHolder holder, int position) {
-        LogItem logItem = logItems.getItem(position);
+        LogItem logItem = filter == null ? logItems.getItem(position) : filteredLogItems.get(position);
         holder.bind(logItem);
     }
 
     @Override
     public int getItemCount() {
-        return logItems.size();
+        return filter == null ? logItems.size() : filteredLogItems.size();
+    }
+
+    public void applyFilter(@Nullable String filter) {
+        this.filter = filter;
+        filteredLogItems = new ArrayList<>();
+        if (filter != null) {
+            for (int i = 0; i < logItems.size(); i++) {
+                LogItem item = logItems.getItem(i);
+                if (item.message.toLowerCase().contains(filter.toLowerCase())) {
+                    filteredLogItems.add(item);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 }
