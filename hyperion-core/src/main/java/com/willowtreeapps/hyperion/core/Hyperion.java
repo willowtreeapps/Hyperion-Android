e476cf1ec9e31b23210aa33055b948f25f3cee76
John Carlson <jawnnypoo@gmail.com>
2019-06-06 11:53:03 -0500

    Fix copy paste error

--- hyperion-core/src/main/java/com/willowtreeapps/hyperion/core/Hyperion.java
+++ hyperion-core/src/main/java/com/willowtreeapps/hyperion/core/Hyperion.java
@@ -72,7 +72,7 @@ public final class Hyperion {
      * Manually trigger the Hyperion menu embedded in the given {@link Activity} to close.
      *
      * @param activity the {@link Activity} containing the menu to close.
-     * @return true if the menu was opened, otherwise false
+     * @return true if the menu was closed, otherwise false
      */
     public static boolean close(Activity activity) {
         requireApplication();
